e45291e3d74c4715c4ee9fff7686773e557137a1
Ludvig Flyckt <contact@ludvigflyckt.com>
2023-11-18 16:15:33 +0100

    Update props nav.tsx

--- nav.tsx
+++ nav.tsx
@@ -1,58 +1,128 @@
+// Navigation.js
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Navigation.scss'; // Import your SCSS file
 
-
-  function Navigation({ activePage, pages, onItemClick }) {
-  // Your component logic here
+function Navigation({
+  activePage,
+  pages,
+  setActivePage,
+  customClass,
+  ulProps,
+  activeStyle,
+  activeClass,
+  icon,
+  openInNewTab,
+  dropdownStyle,
+  dropdownClass,
+  animateDropdown,
+  animationConfigDropdown,
+  renderItem,
+  customLiElement, // Custom <li> element
+  customUlElement, // Custom <ul> element
+  ...customProps // Additional custom props
+}) {
   return (
+    <nav className={`navigation ${customClass}`} {...customProps}>
+      {customUlElement ? (
+        // If custom <ul> is provided, use it
+        React.cloneElement(customUlElement, { className: `navigation ${customClass}` })
+      ) : (
+        // Otherwise, use the default <ul> with ulProps
+        <ul {...ulProps}>
+          {pages.map((page) => (
+            <li
+              key={page.title || page}
+              className={`navigation-title ${page.title ? 'dropdown' : ''} ${
+                page.title && page.title === activePage ? activeClass || 'active' : ''
+              }`}
+              onClick={() => (page.title ? null : setActivePage(page))}
+            >
+              {customLiElement ? (
+                // If custom <li> is provided, use it
+                React.cloneElement(customLiElement(page, activePage === page.title), {
+                  className: `navigation-title ${page.title ? 'dropdown' : ''} ${
+                    page.title && page.title === activePage ? activeClass || 'active' : ''
+                  }`,
+                })
+              ) : (
+                // Otherwise, use the default content
+                <>
+                  {icon && <img src={icon} alt="Icon" />}
+                  <span>{page.title || page}</span>
+                </>
+              )}
 
-
-    <nav className={`navigation ${className}`}>
-      <ul>
-        {pages.map((page) => (
-          <li
-            key={page}
-            className={page === activePage ? 'active' : ''}
-            onClick={() => onItemClick(page)}
-          >
-            {page}
-          </li>
-        ))}
-      </ul>
+              {page.title && (
+                <ul
+                  className={`dropdown-content ${dropdownClass || ''}`}
+                  style={dropdownStyle}
+                  hidden={!animateDropdown && page.title !== activePage}
+                >
+                  {page.items.map((item) => (
+                    <li
+                      key={item}
+                      onClick={() => setActivePage(item)}
+                      className={item === activePage ? 'active' : ''}
+                    >
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 }
 
-
 Navigation.propTypes = {
   activePage: PropTypes.string,
-  pages: PropTypes.arrayOf(PropTypes.string),
-  onItemClick: PropTypes.func,
+  pages: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        items: PropTypes.arrayOf(PropTypes.string).isRequired,
+      }),
+    ])
+  ),
+  setActivePage: PropTypes.func,
+  customClass: PropTypes.string,
+  ulProps: PropTypes.object,
+  activeStyle: PropTypes.object,
+  activeClass: PropTypes.string,
+  icon: PropTypes.string,
+  openInNewTab: PropTypes.bool,
+  dropdownStyle: PropTypes.object,
+  dropdownClass: PropTypes.string,
+  animateDropdown: PropTypes.bool,
+  animationConfigDropdown: PropTypes.object,
+  renderItem: PropTypes.func,
+  customLiElement: PropTypes.func,
+  customUlElement: PropTypes.element,
+  ...customProps, // Include customProps in propTypes
 };
 
 Navigation.defaultProps = {
   activePage: '',
   pages: [],
-  onItemClick: () => {},
+  setActivePage: () => {},
+  customClass: '',
+  ulProps: {},
+  activeStyle: {},
+  activeClass: 'active',
+  icon: '',
+  openInNewTab: false,
+  dropdownStyle: {},
+  dropdownClass: '',
+  animateDropdown: false,
+  animationConfigDropdown: {},
+  renderItem: null,
+  customLiElement: null,
+  customUlElement: null,
 };
 
-
-import React, { useState } from 'react';
-import Navigation from 'my-dynamic-nav'; // Import your npm package
-
-function App() {
-  const [activePage, setActivePage] = useState('Home');
-  const pages = ['Home', 'About', 'Services', 'Contact'];
-
-  const handleItemClick = (page) => {
-    setActivePage(page);
-  };
-
-  return (
-    <div className="App">
-      <Navigation className="navigation-horizontal" activePage={activePage} pages={pages} onItemClick={handleItemClick} />
-    </div>
-  );
-}
-
-export default App;
+export default Navigation;
